d0041b4e205f6aeacc68e4e16bee870cdc8ab1a8
Bill Njoroge <williamsriunge@gmail.com>
2026-03-25 23:46:49 -0400

    implement tree serialize and objects

--- libwyag.py
+++ libwyag.py
@@ -36,6 +36,7 @@ class GitRepository:
                     self.conf.read([cf])
                except Exception as e:
                     print(f"Error while trying to read the config file: {e}")
+                    return
           elif not force:
                raise Exception("config file missing")
           
@@ -43,7 +44,6 @@ class GitRepository:
                vers = int(self.conf.get("core", "repositoryformatversion"))
                if vers != 0:
                     raise Exception(f"Unsupported repository version.")
-               
 class GitObject(ABC):
      """Base class for all objects"""
      def __init__(self, data: bytes | None = None):
@@ -66,6 +66,76 @@ class GitObject(ABC):
      def deserialize(self, data: bytes) -> None:
           """CONVERT To python object from bytes"""
           pass
+
+class GitTreeLeaf:
+     def __init__(self, mode, path, hash):
+          self.mode = mode
+          self.path = path
+          self.hash = hash
+
+def tree_parse(raw):
+    pos = 0
+    max = len(raw)
+    ret = []
+    while pos < max:
+        pos, data = tree_parse_one(raw, pos)
+        ret.append(data)
+
+    return ret
+
+def tree_parse_one(raw, start=0):
+    # Find the space terminator of the mode
+    x = raw.find(b' ', start)
+    assert x-start == 5 or x-start==6
+
+    # Read the mode
+    mode = raw[start:x]
+    if len(mode) == 5:
+        # Normalize to six bytes.
+        mode = b"0" + mode
+
+    # Find the NULL terminator of the path
+    y = raw.find(b'\x00', x)
+    # and read the path
+    path = raw[x+1:y]
+
+    # Read the SHA…
+    raw_sha = int.from_bytes(raw[y+1:y+21], "big")
+    # and convert it into an hex string, padded to 40 chars
+    # with zeros if needed.
+    sha = format(raw_sha, "040x")
+    return y+21, GitTreeLeaf(mode, path.decode("utf8"), sha)
+
+def tree_leaf_sort_key(leaf):
+    if leaf.mode.startswith(b"4"):   # directory
+        return leaf.path + "/"
+    else:
+        return leaf.path
+def tree_serialize(obj):
+    obj.items.sort(key=tree_leaf_sort_key)
+    ret = b''
+    for i in obj.items:
+        ret += i.mode
+        ret += b' '
+        ret += i.path.encode("utf8")
+        ret += b'\x00'
+        sha = int(i.sha, 16)
+        ret += sha.to_bytes(20, byteorder="big")
+    return ret
+
+class GitTree(GitObject):
+    fmt=b'tree'
+
+    def deserialize(self, data):
+        self.items = tree_parse(data)
+
+    def serialize(self):
+        return tree_serialize(self)
+
+    def init(self):
+        self.items = list()
+
+
 
 class GitBlob(GitObject):
      fmt = b'blob'
